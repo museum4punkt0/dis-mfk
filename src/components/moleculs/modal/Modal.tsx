191e413962d545d4592d882a07b77e81a1d2cf4a
brovarny <ext-vsevolod.brovarnyi@here.com>
2023-05-30 13:09:31 +0200

    Add settings block to modal window
    - add open/close animation
    - style settings items from 3D library

--- src/components/moleculs/modal/Modal.tsx
+++ src/components/moleculs/modal/Modal.tsx
@@ -2,6 +2,7 @@ import { MouseEventHandler, useEffect, useState } from "react";
 import { Image3D, Text } from "components/atoms";
 
 import { FaArrowLeft } from "react-icons/fa";
+import { AiFillSetting } from "react-icons/ai";
 import "./style.scss";
 
 type Props = {
@@ -20,12 +21,18 @@ export const Modal = (props: Props) => {
   //Need to change any for different dataType
   const [jsonData, setJsonData] = useState<any | null>(null);
 
+  const [showSettings, setShowSettings] = useState<boolean>(false);
+
+  const openSettings = () => {
+    setShowSettings((oldState) => !oldState);
+  };
+
   useEffect(() => {
     if (jsonData === null) {
       setJsonData(require(`assets/glb/${content}`));
     }
   }, [content, jsonData]);
-  console.log(jsonData);
+  console.warn(jsonData, content);
   return (
     <div className="mfk-modal">
       <div className="mfk-modal--header">
@@ -34,11 +41,17 @@ export const Modal = (props: Props) => {
           <Text type="subtitle">Back to timeline</Text>
         </div>
       </div>
-      <div className="mfk-modal--content">
-        {/* <span className="mfk-modal-close" onClick={onClose}>
-          &#x2716;
-        </span> */}
+      <div
+        className={`mfk-modal--content ${
+          showSettings ? "mfk-settings-open" : ""
+        }`}
+      >
         <div className="mfk-modal--title">{jsonData && jsonData.title}</div>
+        <div className="mfk-modal--settings">
+          <div className="mfk-modal--settings-item" onClick={openSettings}>
+            {showSettings ? <FaArrowLeft /> : <AiFillSetting />}
+          </div>
+        </div>
         {jsonData && <Image3D fileName={content} />}
       </div>
     </div>
