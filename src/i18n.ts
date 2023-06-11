39557505eda9978402a71c7947799be34d66f6b1
brovarny <ext-vsevolod.brovarnyi@here.com>
2023-06-11 13:05:04 +0200

    Separate translations to multiple files
    - add translations for landing
    - split translations by sub categories
    - improve translation configuration

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,45 +1,22 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+import translationEN from "locales/en/translationEN.json";
+import translationDE from "locales/de/translationDE.json";
+import translationGE from "locales/ge/translationGE.json";
+
 const resources = {
-  en: {
-    translation: {
-      logo: "Museum timeline",
-      title: "TECHNICAL MILESTONES",
-      description:
-        "Numerous physical and technical discoveries and inventions have decisively advanced the way we communicate today.",
-    },
-  },
-  de: {
-    translation: {
-      logo: "Zeitleiste des Museums",
-      title: "MEILENSTEINE DER TECHNIK",
-      description:
-        "Zahlreiche physikalische und technische Entdeckungen und Erfindungen haben unsere heutige Art der Kommunikation entschieden vorangebracht.",
-    },
-  },
-  ge: {
-    translation: {
-      logo: "Zeitleiste des Museums",
-      title: "MEILENSTEINE DER TECHNIK",
-      description:
-        "Zahlreiche physikalische und technische Entdeckungen und Erfindungen haben unsere heutige Art der Kommunikation entschieden vorangebracht.",
-    },
-  },
+  en: { translation: translationEN },
+  de: { translation: translationDE },
+  ge: { translation: translationGE },
 };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    //TODO: Check if this is needed
-    lng: localStorage.getItem("lang") || "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
+    fallbackLng: localStorage.getItem("lang") || "en", // need to store lang after refresh
+    debug: true,
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
