d611dfeaee6e2cf5396052db5f99014136ac4412
brovarny <ext-vsevolod.brovarnyi@here.com>
2023-06-30 12:49:10 +0200

    Add new version of smb/display
    - improve Modal component with react based functions instead of pure js
    - add additional functionality for detecting loading state in Viewer
    - add condition to show content after 3D is loaded

--- src/components/atoms/image3D/Image3D.tsx
+++ src/components/atoms/image3D/Image3D.tsx
@@ -1,23 +1,21 @@
-import { Viewer } from "@smb/display";
+import { ReactNode } from "react";
 import { ComponentProps } from "types";
 
 import "./style.scss";
 
 type Props = {
   /**
-   * Name of .glb file
+   * 3D Object based on Viewer from @smb/display library
    */
-  fileName: string;
+  displayObject: ReactNode;
 } & ComponentProps;
 
 export const Image3D = (props: Props) => {
-  const { fileName, identifier } = props;
+  const { displayObject, identifier } = props;
+
   return (
     <div data-testid={identifier} className="mfk-image3D-wrapper">
-      <Viewer
-        src={`assets/glb/${fileName}`}
-        baseResourceUrl={process.env.REACT_APP_API_PATH as string}
-      />
+      {displayObject}
     </div>
   );
 };
